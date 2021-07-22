bb19e0123a46de9170b4949e7bd61d314341cb83
keisuke-jpn <keiske.message@gmail.com>
2021-07-22 17:14:30 +0900

    Changed to linear from sigmoid

--- snake_nn.py
+++ snake_nn.py
@@ -6,10 +6,14 @@ from tensorflow.keras.layers import Dense, Dropout
 
 import numpy as np
 
+from logging import basicConfig, getLogger, DEBUG
+logger = getLogger(__name__)
+logger.setLevel(DEBUG)
+
 class snake_nn:
     def __init__(self):
         self.model = None
-        self.filename = 'snake_nn_model.h5'
+        self.filename = './model/snake_nn_model.h5'
 
     def train(self, data):
         X, y = self.build_train_data(data)
@@ -17,21 +21,21 @@ class snake_nn:
         self.train_model(X, y)
 
     def build_train_data(self, data):
-        X = np.array([i[0] for i in data]).reshape([-1, 8])
+        X = np.array([i[0] for i in data]).reshape([-1, 9])
         y = np.array([i[1] for i in data]).reshape([-1, 1])
-        print(X.shape)
-        print(y.shape)
+        logger.debug(X.shape)
+        logger.debug(y.shape)
         return X, y
 
     def build_model(self):
         model = Sequential()
-        model.add(Dense(10, activation='relu', input_dim=8))
+        model.add(Dense(10, activation='relu', input_dim=9))
         model.add(Dense(10, activation='relu'))
-        model.add(Dense(1, activation='sigmoid'))
-        model.compile(loss='binary_crossentropy',
+        model.add(Dense(1, activation='linear'))
+        model.compile(loss='mean_squared_error',
                   optimizer='adam',
                   metrics=['acc'])
-        print(model.summary())
+        logger.debug(model.summary())
         self.model = model
 
     def train_model(self, X, y):
@@ -43,9 +47,17 @@ class snake_nn:
 
 
 if __name__ == "__main__":
-    game = snake_game(random=True, loop=10, step=10)
+    basicConfig(
+        format='[%(asctime)s] %(name)s %(levelname)s: %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+
+    logger.info('Start snake_nn.py')
+    game = snake_game(auto=True, loop=500, step=100, model_file='./model/snake_nn_model.h5', width=100, height=100)
     game.start()
     snake_nn = snake_nn()
     snake_nn.train(game.snake_observe)
-    new_game = snake_game(auto=True, model_file='snake_nn_model.h5')
+    logger.info('Finish snake_nn.py')
+
+    new_game = snake_game(auto=True, model_file='./model/snake_nn_model.h5')
     new_game.start()
