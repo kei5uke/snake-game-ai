9cce70b07671f14fd2f848c48f28b877c90f7e5b
keisuke-jpn <keiske.message@gmail.com>
2021-07-22 21:37:21 +0900

    Added docstrings, fixed some errors

--- snake_game.py
+++ snake_game.py
@@ -24,22 +24,26 @@ logger = getLogger(__name__)
 logger.setLevel(DEBUG)
 
 class snake_game:
-    def __init__(self, width=600, height=400, random=False, auto=False, manual=False, loop=1, step=None, model_file=None):
+    def __init__(self, width=600, height=400, mode = 'manual', loop=1, step=None, model_file=None):
+        self.mode = mode
+        self.manual = False
+        self.random = False
+        self.auto = False
         self.dis_width = width
         self.dis_height = height
         self.display = pygame.display.set_mode((self.dis_width, self.dis_height))
         self.score = 0
-        self.random = random
-        self.auto = auto
-        self.manual = manual
         self.snake_observe = []
         self.loop = loop
         self.step = step
-
-        if model_file != None:
-            self.model = keras.models.load_model(model_file)
+        self.model_file = model_file
+        self.model = None
 
     def start(self):
+        '''Start snake game based on mode'''
+        if self.mode == 'manual': self.manual = True
+        if self.mode == 'random': self.random = True
+        if self.mode == 'auto': self.auto = True
         pygame.init()
         pygame.display.set_caption('Snake Game')
 
@@ -53,13 +57,13 @@ class snake_game:
                 self.gameLoop()
 
         elif self.auto:
+            self.model = keras.models.load_model(self.model_file)
             for i in range(0, self.loop):
                 logger.info('AUTO GAME {0}'.format(i + 1))
                 self.gameLoop()
 
-        logger.info('FINISHED')
-
     def display_message(self, mode, text):
+        ''' Display message '''
         if mode == 'msg':
             font_style = pygame.font.SysFont("optima", 25)
             mesg = font_style.render(text, True, YELLOW)
@@ -70,42 +74,51 @@ class snake_game:
             self.display.blit(value, [self.dis_width / 3, 0])
 
     def plot_snake(self, snake_list):
+        ''' Plot snake on the map '''
         for x in snake_list:
             pygame.draw.rect(self.display, WHITE, [x[0], x[1], SNAKE_BLOCK, SNAKE_BLOCK])
             logger.debug('SNAKE:{0} {1}'.format(x[0], x[1]))
 
     def plot_food(self, foodx, foody):
+        ''' Plot food on the map '''
         pygame.draw.rect(self.display, GREEN, [foodx, foody, SNAKE_BLOCK, SNAKE_BLOCK])
         logger.debug('FOOD:{0} {1}'.format(foodx, foody))
 
     def generate_snake(self):
+        ''' Generate coordinate of snake '''
         x = round(random.randrange(0, self.dis_width - SNAKE_BLOCK) / 10.0) * 10.0
         y = round(random.randrange(0, self.dis_height - SNAKE_BLOCK) / 10.0) * 10.0
         return x, y
 
     def generate_food(self):
+        ''' Generate coordinate of food '''
         foodx = round(random.randrange(0, self.dis_width - SNAKE_BLOCK) / 10.0) * 10.0
         foody = round(random.randrange(0, self.dis_height - SNAKE_BLOCK) / 10.0) * 10.0
         return foodx, foody
 
     def get_action_array(self, key):
+        ''' One-hot encode of key action '''
         action = np.zeros(4)
         action[key] = 1
         return action
 
     def normalize_vector(self, vec):
+        ''' Normalize vector in -1 ~ 1 '''
         return vec / np.linalg.norm(vec)
 
     def get_angle(self, x1, y1, x1_change, y1_change, foodx, foody):
+        ''' Get andle of the direction of snake and food '''
         snake_dir = self.normalize_vector([x1_change, y1_change])
         food_dir = self.normalize_vector([(foodx - x1), (foody - y1)])
         angle = math.atan2(snake_dir[0] * food_dir[1] - snake_dir[1] * food_dir[0], snake_dir[0] * food_dir[0] + snake_dir[1] * food_dir[1])
         return angle/math.pi
 
     def get_distance_of(self, x1, y1, foodx, foody):
+        ''' Get distance between snake and food '''
         return np.linalg.norm(np.array([(foodx - x1), (foody - y1)]))
 
     def is_direction_blocked(self, x, y, snake_List):
+        ''' Find blocked direction based on current location '''
         blocked_up, blocked_right, blocked_down, blocked_left = False, False, False, False
         if y - 10 == -10:
             blocked_up = True
@@ -128,6 +141,7 @@ class snake_game:
         return np.array([int(blocked_up), int(blocked_right), int(blocked_down), int(blocked_left)])
 
     def get_key_direction(self, key):
+        ''' Get coordinate direction based on key '''
         x1_change, y1_change = 0, 0
         if key == 0:  # up
             y1_change = -SNAKE_BLOCK
@@ -141,12 +155,11 @@ class snake_game:
         elif key == 3:  # left
             x1_change = -SNAKE_BLOCK
             y1_change = 0
-        else:
-            sys.exit("key error: {0}".format(key))
 
         return x1_change, y1_change
 
-    def get_key_action(self, event, x1, y1, foodx, foody, blocked):
+    def get_key_action(self, event):
+        ''' Get key action from keyboard or random number '''
         key = None
         if self.manual:
             if event.type == pygame.KEYDOWN:
@@ -162,21 +175,26 @@ class snake_game:
         elif self.random:
             key = random.randint(0, 3)
 
-        elif self.auto:
-            key = 0
-            prediction = []
-            for i in range(0, 4):
-                action = self.get_action_array(i)
-                x1_change, y1_change = self.get_key_direction(i)
-                angle = self.get_angle(x1, y1, x1_change, y1_change, foodx, foody)
-                prediction.append(self.model.predict(np.hstack((blocked, action, angle)).ravel().reshape(-1, 9)))
-            key = np.argmax(prediction)
-            logger.debug('PREDICTION: {0}'.format(prediction))
-            logger.debug('KEY: {0}'.format(key))
+        return key
+
+    def get_auto_action(self, x1, y1, foodx, foody, blocked):
+        ''' Get predection based on given parameters
+        Only used for auto game mode '''
+        key = 0
+        prediction = []
+        for i in range(0, 4):
+            action = self.get_action_array(i)
+            x1_change, y1_change = self.get_key_direction(i)
+            angle = self.get_angle(x1, y1, x1_change, y1_change, foodx, foody)
+            prediction.append(self.model.predict(np.hstack((blocked, action, angle)).ravel().reshape(-1, 9)))
+        key = np.argmax(prediction)
+        logger.debug('PREDICTION: {0}'.format(prediction))
+        logger.debug('KEY: {0}'.format(key))
 
         return key
 
     def gameLoop(self):
+        ''' Main Game Function '''
         clock = pygame.time.Clock()
         game_over, game_close = False, False
         x1, y1 = self.generate_snake()
@@ -188,9 +206,7 @@ class snake_game:
         action = None
         step = self.step
 
-        # Find blocked direction
         blocked = self.is_direction_blocked(x1, y1, [[x1, y1]])
-        # Distance of food and snake
         distance = self.get_distance_of(x1, y1, foodx, foody)
 
         while not game_over:
@@ -206,9 +222,9 @@ class snake_game:
 
                 for event in pygame.event.get():
                     if event.type == pygame.KEYDOWN:
-                        if event.key == pygame.K_q:
+                        if event.key == pygame.K_q: # Quit game with Q
                             return
-                        elif event.key == pygame.K_c:
+                        elif event.key == pygame.K_c: # Continue with C
                             self.gameLoop()
                             return
 
@@ -217,7 +233,11 @@ class snake_game:
             if event.type == pygame.QUIT:
                 game_over = True
             # Key Action Event
-            key = self.get_key_action(event, x1, y1, foodx, foody, blocked)
+            if self.auto != True:
+                key = self.get_key_action(event)
+            if self.auto == True:
+                key = self.get_auto_action(x1, y1, foodx, foody, blocked)
+
             x1_change, y1_change = self.get_key_direction(key)
             action = self.get_action_array(key)
             angle = self.get_angle(x1, y1, x1_change, y1_change, foodx, foody)
@@ -252,16 +272,15 @@ class snake_game:
             self.plot_food(foodx, foody)  # Plot Food
             self.display_message('score', Length_of_snake - 1)
 
+            # When snake move
             if x1_change != 0 or y1_change != 0:
-                # blocked : Blocked direction
-                # action : Suggested action based on the blocked direction
-                # angle : angle of food and snake (-1 ~ 1)
                 new_distance = self.get_distance_of(x1, y1, foodx, foody)
 
                 if game_close:
                     self.snake_observe.append([np.hstack((blocked, action, angle)).ravel(), -1])
                     logger.debug('BLOCKED:{0} ACTION:{1} DEGREE:{2} LABEL:{3}'.format(blocked, action, math.degrees(angle*math.pi), -1))
                 elif not game_close:
+                    # Comparison of current location and previous location
                     if distance <= new_distance:
                         self.snake_observe.append([np.hstack((blocked, action, angle)).ravel(), 0])
                         logger.debug('BLOCKED:{0} ACTION:{1} DEGREE:{2} LABEL:{3}'.format(blocked, action, math.degrees(angle*math.pi), 0))
@@ -287,7 +306,6 @@ if __name__ == "__main__":
         datefmt='%Y-%m-%d %H:%M:%S'
     )
 
-    game = snake_game(random=True)
+    game = snake_game(mode='manual')
     game.start()
-    logger.debug(game.score)
-    logger.debug(game.snake_observe)
+    logger.debug('GAME SCORE: {}'.format(game.score))
